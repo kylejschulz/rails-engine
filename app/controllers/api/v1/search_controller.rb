96d95a40381c470a141412c50944d8d42a3d0248
kylejschulz <kylschulz@gmail.com>
2021-04-06 08:58:28 -0400

    finish twenty merchants

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -4,14 +4,22 @@ class Api::V1::SearchController < ApplicationController
   end
 
   def merchant_find_one
-    merchant = Merchant.find_one(merchant_find_one_params[:name])
+    merchant = Merchant.find_one(merchant_find_one_params)
     render json: MerchantSerializer.new(merchant), status: 200
+  end
 
+  def item_find_all
+    items = Item.find_all(item_find_all_params)
+    redner json: ItemSerializer.new(items), status: 200
   end
 
   private
 
   def merchant_find_one_params
-    params.permit(:name, )
+    params.permit(:name)
+  end
+
+  def item_find_all_params
+    params.permint(:name, :min_price, :max_price )
   end
 end
